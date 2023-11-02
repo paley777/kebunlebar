5db95de2004893b39a6f66855387b7461449baae
Valleryan Virgil Zuliuskandar <valleryan1212@gmail.com>
2023-11-03 02:04:09 +0700

    update

--- resources/views/landing/layouts/main.blade.php
+++ resources/views/landing/layouts/main.blade.php
@@ -42,7 +42,7 @@
     </style>
     <!-- Back to top button -->
     <button type="button" class="btn btn-light btn-floating btn-lg" id="btn-back-to-top">
-        <?xml version="1.0" encoding="utf-8"?>
+        
 
         <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
         <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
